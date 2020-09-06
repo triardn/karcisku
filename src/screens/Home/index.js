1caa14f8ff5ada2e8c5b8a259a4aa49acbcd481d
Tri Ardini <tr.ardn@gmail.com>
2020-09-06 18:01:06 +0700

    logout wip

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,8 +1,20 @@
-import React from 'react';
-import {View, Text, FlatList, TouchableOpacity} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {
+  View,
+  Text,
+  FlatList,
+  TouchableOpacity,
+  ToastAndroid,
+} from 'react-native';
 import {styles} from './styles';
 import FAIcon from 'react-native-vector-icons/FontAwesome5';
+import MCIcon from 'react-native-vector-icons/MaterialCommunityIcons';
 import data from './data.json';
+
+import Axios from 'axios';
+import Asyncstorage from '@react-native-community/async-storage';
+import api from '../../api/index';
+import {GoogleSignin, statusCode} from '@react-native-community/google-signin';
 
 const Home = ({navigation}) => {
   const formatCurrency = (number) => {
@@ -15,6 +27,7 @@ const Home = ({navigation}) => {
 
     return number;
   };
+
   const renderItem = ({item, index}) => {
     return (
       <View
@@ -45,17 +58,93 @@ const Home = ({navigation}) => {
     );
   };
 
+  const [userInfo, setUserInfo] = useState('');
+  const [isUsingGoogle, setIsUsingGoogle] = useState(false);
+
+  useEffect(() => {
+    async function getToken() {
+      try {
+        const token = await Asyncstorage.getItem('token');
+        // return getVenue(token);
+      } catch (err) {
+        showToast('Gagal mengambil token');
+        console.log('getToken -> err', err);
+      }
+    }
+    getToken();
+    if (isUsingGoogle) {
+      getCurrentUser();
+    }
+  }, []);
+
+  const getCurrentUser = async () => {
+    try {
+      const userInfo = await GoogleSignin.signInSilently();
+      setUserInfo(userInfo);
+    } catch (err) {
+      showToast('Gagal melakukan fetch data user');
+      console.log('getCurrentUser -> err', err);
+    }
+  };
+
+  const showToast = (message) => {
+    ToastAndroid.show(message, ToastAndroid.SHORT);
+  };
+
+  const getVenue = (token) => {
+    Axios.get(`${api}/venues`, {
+      timeout: 20000,
+      headers: {
+        Authorization: 'Bearer' + token,
+      },
+    })
+      .then((res) => {
+        console.log('getVenue -> resp', res);
+      })
+      .catch((err) => {
+        showToast('Gagal melakukan fetch data venues. Silakan cek log');
+        console.log('getVenue -> err', err);
+      });
+  };
+
+  const isSignedIn = async () => {
+    const isSignedIn = await GoogleSignin.isSignedIn();
+    setIsUsingGoogle(isSignedIn);
+  };
+
+  const logout = async () => {
+    try {
+      if (isUsingGoogle) {
+        await GoogleSignin.revokeAccess();
+        await GoogleSignin.signOut();
+      } else {
+        await Asyncstorage.removeItem('token');
+      }
+
+      showToast('Sukses logout dari aplikasi');
+
+      navigation.navigate('Login');
+    } catch (err) {
+      showToast('Gagal logout. Silakan coba beberapa saat lagi');
+      console.log('onLogoutPress -> err', err);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <View style={{flexDirection: 'row', marginTop: 50, marginLeft: 20}}>
+      <View
+        style={{
+          flexDirection: 'row',
+          marginTop: 50,
+          marginLeft: 20,
+          marginRight: 20,
+        }}>
         <FAIcon name="user-circle" size={50} color="#8B0000" />
         <View
           style={{
             marginLeft: 20,
-            // alignItems: 'center',
-            // justifyContent: 'center',
           }}>
-          <Text style={{fontSize: 20}}>Hi, Raisa</Text>
+          <Text style={{fontSize: 20}}>Hi, {userInfo.name}</Text>
           <TouchableOpacity
             style={{
               borderRadius: 10,
@@ -67,6 +156,14 @@ const Home = ({navigation}) => {
             onPress={() => navigation.navigate('Profile')}>
             <Text style={{color: 'white'}}>Edit profile</Text>
           </TouchableOpacity>
+        </View>
+        <View style={{flex: 1, alignItems: 'flex-end'}}>
+          <MCIcon
+            name="logout-variant"
+            size={50}
+            color="#8B0000"
+            onPress={() => logout()}
+          />
         </View>
       </View>
       <View style={{marginTop: 20}}>
