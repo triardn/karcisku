3a3eb55b4b6bed8da906fa2ee4e4e5de7d53f383
Tri Ardini <tr.ardn@gmail.com>
2020-09-12 23:35:21 +0700

    feature - chat

--- src/screens/Chat/index.js
+++ src/screens/Chat/index.js
@@ -1,11 +1,58 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {GiftedChat} from 'react-native-gifted-chat';
+import auth from '@react-native-firebase/auth';
+import database from '@react-native-firebase/database';
 
 const Chat = () => {
+  const [messages, setMessages] = useState([]);
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    const usr = auth().currentUser;
+    setUser(usr);
+    getData();
+
+    return () => {
+      const db = database().ref('messages');
+      if (db) {
+        db.off();
+      }
+    };
+  }, []);
+
+  const getData = () => {
+    database()
+      .ref('messages')
+      .limitToLast(20)
+      .on('child_added', (snapshot) => {
+        const value = snapshot.val();
+        setMessages((previousMessages) =>
+          GiftedChat.append(previousMessages, value),
+        );
+      });
+  };
+
+  const onSend = (msgs = []) => {
+    for (let i = 0; i < msgs.length; i++) {
+      database().ref('messages').push({
+        _id: msgs[i]._id,
+        created: database.ServerValue.TIMESTAMP,
+        text: msgs[i].text,
+        user: msgs[i].user,
+      });
+    }
+  };
+
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Chat page</Text>
-    </View>
+    <GiftedChat
+      messages={messages}
+      onSend={(msgs) => onSend(msgs)}
+      user={{
+        _id: user.uid,
+        name: user.email,
+        avatar: 'https://ui-avatars.com/api/?name=user',
+      }}
+    />
   );
 };
 
