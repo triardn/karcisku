1d083825be968aaabf274d5dd27428b4bafae290
Tri Ardini <tr.ardn@gmail.com>
2020-09-18 22:28:30 +0700

    push notif

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React, {useEffect} from 'react';
-import AppNavigation from './src/routes';
+import AppNavigation from './src/index';
 import firebase from '@react-native-firebase/app';
+import OneSignal from 'react-native-onesignal';
 
 var firebaseConfig = {
   apiKey: 'AIzaSyC98Vjg0ymMNGG3rzIBNToayb22yGo4v9A',
@@ -20,6 +21,76 @@ if (!firebase.apps.length) {
 }
 
 const App: () => React$Node = () => {
+  useEffect(() => {
+    console.log('ahai');
+    OneSignal.setLogLevel(6, 0);
+
+    OneSignal.init('59a44d5f-01d7-4195-9ab5-2221dd6fa5be', {
+      kOSSettingsKeyAutoPrompt: false,
+      kOSSettingsKeyInAppLaunchURL: false,
+      kOSSettingsKeyInFocusDisplayOption: 2,
+    });
+    OneSignal.inFocusDisplaying(2);
+
+    OneSignal.addEventListener('received', onReceived);
+    OneSignal.addEventListener('opened', onOpened);
+    OneSignal.addEventListener('ids', onIds);
+
+    // codePush.sync(
+    //   {
+    //     updateDialog: true,
+    //     installMode: codePush.InstallMode.IMMEDIATE,
+    //   },
+    //   syncStatus,
+    // );
+
+    return () => {
+      OneSignal.removeEventListener('received', onReceived);
+      OneSignal.removeEventListener('opened', onOpened);
+      OneSignal.removeEventListener('ids', onIds);
+    };
+  }, []);
+
+  // const syncStatus = (status) => {
+  //   switch (status) {
+  //     case codePush.SyncStatus.CHECKING_FOR_UPDATE:
+  //       console.log('Checking for updates');
+  //       break;
+  //     case codePush.SyncStatus.DOWNLOADING_PACKAGE:
+  //       console.log('Downloading package');
+  //       break;
+  //     case codePush.SyncStatus.UP_TO_DATE:
+  //       console.log('Up to date');
+  //       break;
+  //     case codePush.SyncStatus.INSTALLING_UPDATE:
+  //       console.log('Installing update');
+  //       break;
+  //     case codePush.SyncStatus.UPDATE_INSTALLED:
+  //       Alert.alert('Notification', 'Update installed');
+  //       break;
+  //     case codePush.SyncStatus.AWAITING_USER_ACTION:
+  //       console.log('Awaiting user action');
+  //       break;
+  //     default:
+  //       break;
+  //   }
+  // };
+
+  const onReceived = (notification) => {
+    console.log('Notification received: ', notification);
+  };
+
+  const onOpened = (openResult) => {
+    console.log('Message: ', openResult.notification.payload.body);
+    console.log('Data: ', openResult.notification.payload.additionalData);
+    console.log('isActive: ', openResult.notification.isAppInFocus);
+    console.log('openResult: ', openResult);
+  };
+
+  const onIds = (device) => {
+    console.log('Device info: ', device);
+  };
+
   return <AppNavigation />;
 };
 
